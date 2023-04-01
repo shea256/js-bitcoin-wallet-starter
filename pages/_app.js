22c06013d976e993e318a7fbde63fcbe968c3633
Ryan Shea <ryan@shea.io>
2023-04-01 11:59:35 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css'
+
 import '@/components/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+import dynamic from "next/dynamic"
+
+export function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default dynamic(() => Promise.resolve(App), {
+  ssr: false,
+})
